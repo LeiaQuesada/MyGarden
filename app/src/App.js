7126ab7fe7d213d3882251811496a0a40f1f70c7
Leia Quesada <leiaquesada@gmail.com>
2021-05-14 12:49:44 -0700

    To protect routes with Auth0
    use auth0 hooks, follow https://github.com/auth0/auth0-react/blob/master/EXAMPLES.md#1-protecting-a-route-in-a-react-router-dom-app

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
 
 import "./styles.css";
+import { withAuthenticationRequired, useAuth0 } from "@auth0/auth0-react";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
 import Header from "./components/Header";
+import PlantRecommendations from "./components/PlantRecommendations";
 import SignIn from "./components/SignIn";
+import UserProfile from "./components/UserProfile";
+export const history = createBrowserHistory();
+
+const ProtectedRoute = ({ component, ...args }) => (
+  <Route component={withAuthenticationRequired(component)} {...args} />
+);
 
 export default function App() {
+  const { isLoading, error } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <Header />
-      <SignIn />
+      {error && <div>{error.message}</div>}
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={SignIn} />
+          <ProtectedRoute path="/profile" component={UserProfile} />
+          <ProtectedRoute
+            path="/recommendations"
+            component={PlantRecommendations}
+          />
+        </Switch>
+      </Router>
       <footer>Disclaimer © 2021</footer>
     </>
   );

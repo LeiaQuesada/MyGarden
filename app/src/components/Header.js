81ca8c746e54434571aae2b1b3863d20906b322f
Leia Quesada <leiaquesada@gmail.com>
2021-05-14 12:50:13 -0700

    Remove authentication conditionals
    Components now checked at App.js, no longer need conditionals whether or not the user is authenticated.

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,54 +1,35 @@
 import React from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../styles.css";
-import PlantRecommendations from "./PlantRecommendations";
-import UserProfile from "./UserProfile";
 
 export default function Header() {
   const { logout, user, isAuthenticated } = useAuth0();
 
   return (
-    <>
-      <Router>
-        <div className="header">
-          <h1>MyGarden</h1>
-          {isAuthenticated ? (
-            <>
-              <main>
-                <nav>
-                  <Link to="/profile">Profile</Link>
-                </nav>
-              </main>
-              <div id="user-info">
-                <div id="left">
-                  <img id="userpic" src={user.picture} alt={user.name} />
-                </div>
-                <div id="right">
-                  <button
-                    onClick={() => logout({ returnTo: window.location.origin })}
-                  >
-                    Log Out
-                  </button>
-                </div>
-              </div>
-            </>
-          ) : null}
-        </div>
-        <Switch>
-          {/* <Route exact path="/">
-            <UserProfile />
-          </Route> */}
-          <Route exact path="/profile">
-            <UserProfile />
-          </Route>
-          <Route path="/recommendations">
-            <PlantRecommendations />
-          </Route>
-        </Switch>
-      </Router>
-    </>
+    <div className="header">
+      <h1>MyGarden</h1>
+      {isAuthenticated ? (
+        <>
+          <main>
+            <nav></nav>
+          </main>
+          <div id="user-info">
+            <div id="left">
+              <img id="userpic" src={user.picture} alt={user.name} />
+            </div>
+            <div id="right">
+              <button
+                onClick={() => logout({ returnTo: window.location.origin })}
+              >
+                Log Out
+              </button>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </div>
   );
 }
--- app/src/components/SignIn.js
+++ app/src/components/SignIn.js
@@ -5,29 +5,21 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "../styles.css";
 
 export default function SignIn() {
-  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   return (
-    <>
-      {isLoading ? (
-        <>
-          <div>Loading ...</div>
-        </>
-      ) : !isAuthenticated ? (
-        <div>
-          <h2>Welcome to MyGarden, your edible garden planning tool!</h2>
-          <p>
-            MyGarden allows you to plot an edible garden, whether you're growing
-            in a small indoor space or a large outdoor plot. Studies show that
-            gardening may improve mental and physical health. This app serves to
-            remove the hassles of researching how to grow vegetables and herbs
-            and provides gardening recommendations based on your selections.
-            This way, you can focus on the more relaxed motions of building and
-            watching your efforts literally come to fruition.
-          </p>
-          <button onClick={loginWithRedirect}>Sign up / Log In</button>
-        </div>
-      ) : null}
-    </>
+    <div>
+      <h2>Welcome to MyGarden, your edible garden planning tool!</h2>
+      <p>
+        MyGarden allows you to plot an edible garden, whether you're growing in
+        a small indoor space or a large outdoor plot. Studies show that
+        gardening may improve mental and physical health. This app serves to
+        remove the hassles of researching how to grow vegetables and herbs and
+        provides gardening recommendations based on your selections. This way,
+        you can focus on the more relaxed motions of building and watching your
+        efforts literally come to fruition.
+      </p>
+      <button onClick={loginWithRedirect}>Sign up / Log In</button>
+    </div>
   );
 }

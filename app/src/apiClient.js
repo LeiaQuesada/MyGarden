81ebcf1270861b3106978eb0bde4d1d4996ed8d5
Leia Quesada <leiaquesada@gmail.com>
2021-05-14 15:56:11 -0700

    Refactor code for dynamic content.
    Separate out api requests into apiClient. Add conditional to make the home screen display option to update the user profile only if the user has never updated their information. After login, apply condition to show plot data.

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -16,18 +16,14 @@ export const addTask = async (name) => {
   return await response.json();
 };
 
-//  export const testAuth = async (token) => {
-//    // FIXME: get JWT (need to resolve React-related error)
-//    const { getAccessTokenSilently } = useAuth0();
-//    const token = await getAccessTokenSilently();
-
-//    const response = await fetch("/api/test", {
-//      headers: {
-//        Authorization: `Bearer ${token}`,
-//      },
-//    });
-//    return await response.json();
-// };
+export const getUser = async (token, email) => {
+  const response = await fetch(`/api/user/${email}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return await response.json();
+};
 
 export const getPlants = async (token, zone) => {
   const response = await fetch(`/api/plants/${zone}`, {
---
+++ app/src/components/Home.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+import { history } from "../App";
+import * as apiClient from "../apiClient";
+import "../styles.css";
+
+export default function Home() {
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  useEffect(() => {
+    async function showProfile() {
+      const token = await getAccessTokenSilently();
+      const userObj = await apiClient.getUser(token, user.email);
+      if (userObj.showprofile) {
+        history.push({
+          pathname: "/profile",
+        });
+      }
+    }
+    if (isAuthenticated) {
+      showProfile();
+    }
+  }, [user, getAccessTokenSilently, isAuthenticated]);
+
+  return (
+    <>
+      {isLoading ? (
+        <>
+          <div>Loading ...</div>
+        </>
+      ) : !isAuthenticated ? (
+        <div>
+          <h2>Welcome to MyGarden, your edible garden planning tool!</h2>
+          <p>
+            MyGarden allows you to plot an edible garden, whether you're growing
+            in a small indoor space or a large outdoor plot. Studies show that
+            gardening may improve mental and physical health. This app serves to
+            remove the hassles of researching how to grow vegetables and herbs
+            and provides gardening recommendations based on your selections.
+            This way, you can focus on the more relaxed motions of building and
+            watching your efforts literally come to fruition.
+          </p>
+          <button onClick={loginWithRedirect}>Sign up / Log In</button>
+        </div>
+      ) : (
+        <h3>Plot Details </h3>
+      )}
+    </>
+  );
+}

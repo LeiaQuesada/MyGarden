d01f3965dbb0e06839c51d8d8f2ec32af91b8e90
George Song <george@monozuku.com>
2021-05-19 14:22:19 -0700

    fix: remove unnecessary import

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../styles.css";
 
